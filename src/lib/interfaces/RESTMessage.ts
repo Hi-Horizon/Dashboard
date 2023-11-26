8ba8e6bdd84802e54f3a3e1b39d3a6d6adf0c035
sdrent <S.Drent@student.tudelft.nl>
2023-11-26 16:35:46 +0100

    added a timerange selection feature to graphviewer

---
+++ src/lib/interfaces/RESTMessage.ts
@@ -0,0 +1,4 @@
+export interface RESTMessage {
+    result: any
+    status: number
+}
--- src/routes/user/graphViewer/[graph]/+page.server.ts
+++ src/routes/user/graphViewer/[graph]/+page.server.ts
@@ -1,10 +1,14 @@
 import { getAllDataPointsFromAxes } from "$lib/server/queries/graphs/getDataPoints";
 import { fetchReadStatisticsTypes } from "$lib/server/queries/settings/ReadStatistics"
+import { fetchDataSet } from "$lib/server/queries/graphs/fetchDataSet";
 import type { Actions } from "./$types";
+
+//TODO: fetch recording Interval
 
 export const load = async ({ params }) => {
     return {
         graphName: params.graph,
         readStatisticTypes: await fetchReadStatisticsTypes(),
+        recordingInterval: await fetchDataSet(-1) 
     }
 }
