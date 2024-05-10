535c0b011c42d15ba58e3753d5b1eb7592c9075a
Jitse0 <jitse.ritskes@gmail.com>
2024-05-10 20:20:45 +0200

    Added Manual Estela Map

--- src/routes/user/dashboard/+page.server.ts
+++ src/routes/user/dashboard/+page.server.ts
@@ -1,9 +1,16 @@
 import { fetchLatestData } from '$lib/server/queries/dashboard/dataFrame.js';
 import { fetchReadStatisticsTypes } from '$lib/server/queries/settings/ReadStatistics.js';
+/** @type {import('./$types').Actions} */
 
 export async function load() {
     return {
         dataFrame: fetchLatestData(),
         dataFrameStructure: fetchReadStatisticsTypes()
+    }   
+}
+export const actions = {
+    submit: async ({request}) => {
+        const link = await request.formData
+        return {success : true}
     }
 }
--- src/routes/user/dashboard/+page.svelte
+++ src/routes/user/dashboard/+page.svelte
@@ -19,6 +19,11 @@ socket.on('dataUpdate', (message) => {
 let statusColor: string = "";
 const currentDate = new Date();
 let timeSinceLastFrame: Writable<number> = writable(Math.round(currentDate.getTime()/1000)+7200 - boatData.UnixTime);
+
+//********************ESTELA LINK HERE ************************/
+let link: string = "https://estela.co/nl/race/13451/solar-racing-2024-nk-groenleven-zonnebootrace-endurance";
+//********************ESTELA LINK HERE ************************/
+
 setInterval(()=> {
     const currentDate = new Date();
     timeSinceLastFrame.set(Math.round(currentDate.getTime()/1000)+7200 - boatData.UnixTime);
@@ -37,40 +42,44 @@ $: {
 
 <div class="space-y-3">
     <div class="flex justify-evenly space-x-3">
-    <div class="rounded border-separate bg-stone-800 flex-1">
-    {#each dataFrameStructure as readStatistic, index}
-        {#if readStatistic.name !== "UnixTime"}
-            {#if index < (dataFrameStructure.length/2)}
-                <div class="even:bg-stone-700 p-3 last:rounded-b">
-                    <p class="font-bold">{readStatistic.name}</p>
-                    <div class="flex flex-row items-end space-x-1">
-                        <p class="text-4xl">{boatData[readStatistic.abbreviation].toFixed(2)}</p>
-                        <p class="text-stone-400">{readStatistic.unit}</p>
+        <div class="rounded border-separate bg-stone-800 flex-1">
+        {#each dataFrameStructure as readStatistic, index}
+            {#if readStatistic.name !== "UnixTime"}
+                {#if index < (dataFrameStructure.length/2)}
+                    <div class="even:bg-stone-700 p-3 last:rounded-b">
+                        <p class="font-bold">{readStatistic.name}</p>
+                        <div class="flex flex-row items-end space-x-1">
+                            <p class="text-4xl">{boatData[readStatistic.abbreviation].toFixed(2)}</p>
+                            <p class="text-stone-400">{readStatistic.unit}</p>
+                        </div>
                     </div>
-                </div>
+                {/if}
             {/if}
-        {/if}
-    {/each}
-    </div>
-    <div class="rounded border-separate bg-stone-800 flex flex-col flex-1" >
-    {#each dataFrameStructure as readStatistic, index}
-        {#if readStatistic.name !== "UnixTime"}
-            {#if index >= (dataFrameStructure.length/2)}
-                <div class="even:bg-stone-700 p-3 last:rounded-b">
-                    <p class="font-bold">{readStatistic.name}</p>
-                    <div class="flex flex-row items-end space-x-1">
-                        <p class="text-4xl">{boatData[readStatistic.abbreviation].toFixed(2)}</p>
-                        <p class="text-stone-400">{readStatistic.unit}</p>
-                    </div>
-                </div>
-            {/if}
-        {/if}
-    {/each}
-    {#if dataFrameStructure.length % 2 === 1}
-        <div class="even:bg-stone-700 p-3 last:rounded-b flex-grow">
+        {/each}
         </div>
-    {/if}
-    </div>
+        <div class="rounded border-separate bg-stone-800 flex flex-col flex-1" >
+        {#each dataFrameStructure as readStatistic, index}
+            {#if readStatistic.name !== "UnixTime"}
+                {#if index >= (dataFrameStructure.length/2)}
+                    <div class="even:bg-stone-700 p-3 last:rounded-b">
+                        <p class="font-bold">{readStatistic.name}</p>
+                        <div class="flex flex-row items-end space-x-1">
+                            <p class="text-4xl">{boatData[readStatistic.abbreviation].toFixed(2)}</p>
+                            <p class="text-stone-400">{readStatistic.unit}</p>
+                        </div>
+                    </div>
+                {/if}
+            {/if}
+        {/each}
+        {#if dataFrameStructure.length % 2 === 1}
+            <div class="even:bg-stone-700 p-3 last:rounded-b flex-grow">
+            </div>
+        {/if}
+        </div>
+        <div>
+            Current/Most recent race:
+            <iframe width="560" height="315" src={link} frameborder="0" allowfullscreen></iframe>
+        </div>
     </div>
     <div class="p-3 rounded bg-stone-800 self-start w-fit">
         <div class="font-bold">Status</div>
