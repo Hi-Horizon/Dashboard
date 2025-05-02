e0fc738dbfc00e2823bc59f3b3d6c471e8678b5a
SenneDrent <sennedrent@gmail.com>
2025-05-02 22:48:37 +0200

    add datadescription table to replace readstatistics

--- webapp/src/lib/server/queries/settings/ReadStatistics.ts
+++ webapp/src/lib/server/queries/settings/ReadStatistics.ts
@@ -14,12 +14,12 @@ export function parseOperationReadStatistic(change: SettingsLocalChange) {
 }
 
 export async function fetchReadStatisticsTypes(): Promise<ReadStatisticType[]> {
-    const stmt = db.prepare("SELECT * FROM ReadStatisticTypes");
+    const stmt = db.prepare("SELECT * FROM DataDescription");
     const rawResult:any[] = stmt.raw().all();
 
     const processedResult: ReadStatisticType[] = [];
     rawResult.forEach((item:string[]) => {
-        let newReadStatisticType: ReadStatisticType = {name: item[0], abbreviation: item[1], quantity: item[2], unit: item[3], display: item[4]}
+        let newReadStatisticType: ReadStatisticType = {id: item[0], name: item[1], abbreviation: item[2], quantity: item[3], unit: item[4], display: item[5]}
         processedResult.push(newReadStatisticType);
     });
     return processedResult;
@@ -29,30 +29,29 @@ export async function fetchReadStatisticsTypes(): Promise<ReadStatisticType[]> {
 export function addReadStatisticsType(newRow:ReadStatisticType)  {
         preventSpecialCharacters(newRow.abbreviation)
         //adds info about statistic to ReadStatisticTypes table
-        const insertRow = db.prepare('INSERT INTO ReadStatisticTypes VALUES (@name, @abbreviation, @quantity, @unit, @display);');
-        const insertInfo = insertRow.run({name: newRow.name, abbreviation: newRow.abbreviation, quantity: newRow.quantity, unit: newRow.unit, display: 1});
+        const insertRow = db.prepare('INSERT INTO DataDescription (name, tag, quantity, unit, display) VALUES (@name, @tag, @quantity, @unit, @display);');
+        const insertInfo = insertRow.run({name: newRow.name, tag: newRow.abbreviation, quantity: newRow.quantity, unit: newRow.unit, display: 1});
 
         //adds column to the data table
         // const addColumn = db.prepare('ALTER TABLE Data ADD @colName INTEGER NOT NULL DEFAULT 0');
         // const AddColumnInfo = addColumn.run({colName: newRow.abbreviation});
 
-        //bind parameters dont work so fuck you and sqlInjections
         const addColumn = db.prepare('ALTER TABLE Data ADD '+ newRow.abbreviation +' INTEGER NOT NULL DEFAULT 0;');
         const AddColumnInfo = addColumn.run();
 }
 
 export function deleteReadStatisticsType(toDelete:string) {
         //get abbreviation to delete the column in the Data table
-        const getAbbreviationToDelete = db.prepare('SELECT abbreviation FROM ReadStatisticTypes WHERE name = ?;');
+        const getAbbreviationToDelete = db.prepare('SELECT tag FROM DataDescription WHERE name = ?;');
         const columnToDelete:any = getAbbreviationToDelete.get(toDelete);
 
         //delete row from the ReadStatisticTypes table
-        const insertRow = db.prepare('DELETE FROM ReadStatisticTypes WHERE name = ?;');
+        const insertRow = db.prepare('DELETE FROM DataDescription WHERE name = ?;');
         const insertInfo = insertRow.run(toDelete);
 
         preventSpecialCharacters(columnToDelete.abbreviation)
         //deletes column in the Data table
-        const addColumn = db.prepare('ALTER TABLE Data DROP COLUMN '+ columnToDelete.abbreviation +';');
+        const addColumn = db.prepare('ALTER TABLE Data DROP COLUMN '+ columnToDelete.tag +';');
         const AddColumnInfo = addColumn.run();
 }
 
