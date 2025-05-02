55e30ffd73d8b3fcb573dd5f207917fdbcf0b09b
SenneDrent <sennedrent@gmail.com>
2025-05-02 21:58:13 +0200

    first implementation static adapter

--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { io } from "socket.io-client";
+    import { enhance } from '$app/forms';
     import { onMount } from "svelte";
     import { cubicOut } from "svelte/easing";
 import { fly } from "svelte/transition";
@@ -22,7 +23,7 @@ onMount(()=>{
     {/if}
     <div class="flex flex-col bg-stone-900 z-10 pb-10 mt-3">
         <h1 class="font-bold text-2xl pb-5">Hi-Horizon Racing Team Telemetry Dashboard</h1>
-        <form class="flex flex-col space-y-3 w-1/2 self-center" method="POST" action="?/login">
+        <form class="flex flex-col space-y-3 w-1/2 self-center" method="POST" action="?/login" use:enhance>
             <input class="text-stone-900 rounded text-center" placeholder="username" name="username" value="admin">
             <input type="password" class="text-stone-900 rounded text-center" placeholder="password" name="password" value="test">
             <button type="submit" class="bg-red-600 hover:bg-red-500 rounded">Login</button>
