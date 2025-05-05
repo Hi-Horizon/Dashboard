c4f52b21ae218ae815a1e9402235db60ab9dfbd2
SenneDrent <sennedrent@gmail.com>
2025-05-05 17:21:56 +0200

    clean up the dashboard and create components for list, buttons, estela, cells

---
+++ webapp/src/lib/Components/cell.svelte
@@ -0,0 +1,3 @@
+<div class="rounded bg-stone-800 p-3">
+    <slot></slot>
+</div>
---
+++ webapp/src/routes/user/dashboard/estela.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Button from "../../../lib/Components/button.svelte";
+
+let inputLink: string = "";
+let link: string = "";
+
+function changeLink() {
+    link = inputLink;
+}
+</script>
+
+<div class="rounded bg-stone-800 p-3">
+    <!-- Current/Most recent race: -->
+    <div class="rounded bg-stone-900">
+        <iframe title="estela tracker" width="560" height="315" src={link} frameborder="0" allowfullscreen>track the race by filling in a link down below</iframe>
+    </div>
+    <div class="flex pt-3 space-x-1">
+        <input type="text" bind:value={inputLink} placeholder="https://estela.co/..." class="rounded bg-stone-700 px-2 flex-1 placeholder:italic">
+        <Button props={{onclick:changeLink}}>Watch</Button>
+    </div>
+</div>
