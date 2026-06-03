9a56acabf5ce45c6031cbdc10b5b9f7ea13c6c5b
Senne Drent <sennedrent@gmail.com>
2026-06-03 17:26:09 +0200

    Hot fix for connection stores being unloaded

---
+++ webapp/src/lib/fetchConnectionStores.ts
@@ -0,0 +1,27 @@
+import type { Writable } from "svelte/store";
+import { db } from "./IOconnections/DBO/databaseObject";
+
+// fetch initial values of the connection stores, important since these stores define that incoming data
+export async function fetchConnectionStores(datadescriptionStore: Writable<any[]>, canSchemaStore: Writable<any>, liveData: Writable<any>) {
+    const dataDescription: any[] = await db.select('SELECT * FROM DataDescription')
+    
+    //fill the canschema values
+    const canSchema = Object
+        .groupBy(dataDescription, ({ CANid }) => CANid)
+        
+    // sort on CAN position
+    Object.keys(canSchema).forEach(key => {
+        canSchema[key] = canSchema[key]?.sort((a: any, b: any) => a.CANmsgPosition - b.CANmsgPosition)
+    });
+    
+    // fetch time of last update
+    const lastMsgTime: any[] = await db.select('SELECT max(UnixTime) as UnixTime FROM Data')
+    
+    //update all stores
+    datadescriptionStore.set(dataDescription);
+    canSchemaStore.set(canSchema)
+    liveData.update((xs: any) => {
+        xs["UnixTime"] = lastMsgTime[0].UnixTime
+        return xs
+    })
+}
--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -2,30 +2,11 @@
     import { goto } from "$app/navigation";
     import { onMount } from "svelte";
     import { canSchema, datadescription, liveData } from "./ConnectionStores";
-    import { db } from "../lib/IOconnections/DBO/databaseObject";
+    import { fetchConnectionStores } from "$lib/fetchConnectionStores";
 
     //fetch initial values for canSchema and DataDescription
     onMount(async () => {
-        $datadescription = await db.select('SELECT * FROM DataDescription');
-
-        //fill the tagToIdDict
-        const tagToIdDict: any = {};
-        $datadescription.map((x) => {
-            tagToIdDict[x.tag] = x.id
-        })
-        //fill the canschema values
-        $canSchema = Object.groupBy($datadescription, ({ CANid }) => CANid)
-        // sort on CAN position
-        Object.keys($canSchema).forEach(key => {
-            $canSchema[key] = $canSchema[key].sort((a: any, b: any) => a.CANmsgPosition - b.CANmsgPosition)
-        });
-
-        // fetch time of last update
-        const lastMsgTime: any[] = await db.select('SELECT max(UnixTime) as UnixTime FROM Data')
-        liveData.update((xs: any) => {
-            xs["UnixTime"] = lastMsgTime[0].UnixTime
-            return xs
-        })
+        await fetchConnectionStores(datadescription, canSchema, liveData);
     })
 
     goto("./dashboard")
