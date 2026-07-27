1552f0174c7ba43421fe4593aa9db87cfa09580b
SenneDrent <sennedrent@gmail.com>
2026-07-27 09:34:17 +0200

    button component light mode hover quick fix

--- webapp/src/lib/Components/button.svelte
+++ webapp/src/lib/Components/button.svelte
@@ -2,9 +2,9 @@
     export let props: any = {}
     export let onclick: any = () => {}
     const colour: string = (typeof props.colour === 'undefined') ? "bg-stone-300 dark:bg-stone-700" : props.colour;
-    const hoverColour: string = (typeof props.hoverColour === 'undefined') ? "bg-stone-600" : props.hoverColour;
+    const hoverColour: string = (typeof props.hoverColour === 'undefined') ? "bg-stone-400" : props.hoverColour;
 </script>
 
-<button on:click={onclick} class="text-center {colour} hover:{hoverColour} rounded flex justify-center py-2 px-4">
+<button on:click={onclick} class="text-center {colour} hover:{hoverColour} dark:hover:bg-stone-600 rounded flex justify-center py-2 px-4">
     <slot/>
 </button>
